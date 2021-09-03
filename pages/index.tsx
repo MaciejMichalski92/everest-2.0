6944d25fb90f7656623ea3a27fdc81c32ede5670
Maciej Michalski <maciejmichalski92@gmail.com>
2021-09-04 00:05:45 +0200

    adding module path aliases, change the root of components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import FirstHeader from '../components/atoms/firstHeader';
+import FirstHeader from '@/components/atoms/firstHeader';
+import SecondHeader from '@/components/atoms/secondHeader';
+import Paragraph from '@/components/atoms/paragraph';
 
-export default function Home(): JSX.Element {
-  return <FirstHeader>HA 1</FirstHeader>;
-}
+ const Home =(): JSX.Element=>(
+		<>
+			<FirstHeader>HA 5</FirstHeader>
+			<SecondHeader>Ha 2</SecondHeader>
+			<Paragraph>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquam neque distinctio similique architecto. Dicta, saepe sint aliquam aliquid distinctio id, quam illo iste voluptas ipsa labore aspernatur? Numquam, debitis!</Paragraph>
+		</>
+	);
+
+export default Home;
