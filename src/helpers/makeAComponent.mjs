ae5ed6535bf187cff2157153260976bb876d2b2d
Maciej Michalski <maciejmichalski92@gmail.com>
2021-09-27 22:01:12 +0200

    makeAComponent helper script

---
+++ src/helpers/makeAComponent.mjs
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+
+const dirPath = `/src/components/${
+  process.argv[2] ? process.argv[2] : 'NewComponent'
+}/${process.argv[3] ? process.argv[3] : 'NewComponent'}`;
+const componentName = process.argv[3] || 'NewComponent';
+const contentIndex = `export { ${componentName} as default } from './${componentName}';`;
+const contentStyled = `import styled from 'styled-components';\nexport const Styled${componentName} = styled`;
+const contentTypes = `export interface ${componentName}Types {};`;
+const contentComponent = `import React, { ReactElement } from 'react';\nexport const ${componentName} = ({}: ${componentName}Types): ReactElement => {};`;
+
+const createDir = (dirPath) => {
+  fs.mkdirSync(`${process.cwd()}${dirPath}`, { recursive: true }, (err) => {
+    if (err) console.error(err);
+    else console.log('new dir has been made');
+  });
+};
+
+const createFile = (filePath, fileName, fileContent) => {
+  fs.writeFile(
+    `${process.cwd()}/${filePath}/${fileName}`,
+    fileContent,
+    (err) => {
+      if (err) console.error(err);
+      else console.log(`new ${fileName} has been made`);
+    }
+  );
+};
+
+createDir(dirPath);
+createFile(dirPath, 'index.ts', contentIndex);
+createFile(dirPath, `${componentName}.tsx`, contentComponent);
+createFile(dirPath, `${componentName}.types.ts`, contentTypes);
+createFile(dirPath, `${componentName}.styled.ts`, contentStyled);
